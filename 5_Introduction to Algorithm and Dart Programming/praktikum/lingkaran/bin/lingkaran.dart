97d9136a895df36ff72c171b6c6626c052ef8fe2
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2022-08-29 17:24:22 +0700

    Hapus section 5

--- 5_Introduction to Algorithm and Dart Programming/praktikum/lingkaran/bin/lingkaran.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  final phi = 22 / 7;
-  var r = 14;
-  var luas = phi * r * r;
-  print("Luas lingkaran dengan jari-jari $r cm adalah $luas cm2");
-}
-
--- 5_Introduction to Algorithm and Dart Programming/praktikum/lingkaran/bin/variabel.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  var namaDepan = "Zakiyuddin";
-  var namaBelakang = "Rahman";
-  var namaPanggilan = "Zaki";
-  print("Hai, nama saya $namaDepan $namaBelakang, biasa dipanggil $namaPanggilan");
-}
