3cbc5755e6f0012ee241483c91342945da7e6016
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2022-09-03 16:17:12 +0700

    Revert "Membuat tugas praktikum section 8"
    This reverts commit 0d728ea6baf840220767a9457740b55af04261eb.

--- 8_Advance Function, Async-Await/praktikum/bin/map.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  var peserta = [['nama', 'Zaki'], ['umur', 20]];
-  var map = {};
-  for (var i = 0; i < peserta.length; i++) {
-    map[peserta[i][0]] = peserta[i][1];
-  }
-  print(map);
-}
