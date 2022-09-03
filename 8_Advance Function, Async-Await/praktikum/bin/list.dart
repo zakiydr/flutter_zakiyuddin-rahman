1b8931c8fa26f9de085bc43a2f7525a7dfb0ac6c
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2022-09-03 16:57:13 +0700

    Menambahkan screenshot dan revisi praktikum

--- 8_Advance Function, Async-Await/praktikum/bin/list.dart
+++ 8_Advance Function, Async-Await/praktikum/bin/list.dart
@@ -1,5 +1,18 @@
-import 'package:praktikum/praktikum.dart' as praktikum;
+Future<void> kali() async {
+  var list = [5, 10, 15, 20];
+  var pengali = 5;
+  
+  await Future.delayed(Duration(seconds: 1), (){
+  for (var dataList = 0; dataList < list.length; dataList ++){
+    print(multiply(list[dataList], pengali));
+    }
+    });
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${praktikum.calculate()}!');
+void main() async {
+  kali();
+}
+
+multiply(int data, int pengali){
+  return data * pengali;
 }
