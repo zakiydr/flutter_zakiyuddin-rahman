ec5245e23d959ad625922f079912df9bdb833502
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2022-09-10 17:15:52 +0700

    Membuat file praktikum nomor 1

---
+++ 11_Object Oriented Programming-2/praktikum/bin/volume.dart
@@ -0,0 +1,51 @@
+class BangunRuang {
+  int panjang = 0;
+  int lebar = 0;
+  int tinggi = 0;
+
+  BangunRuang()
+  {
+    panjang = 0;
+    lebar = 0;
+    tinggi = 0;
+  }
+  int volume = 0;
+  void hitungVolume() {
+    volume = panjang * lebar * tinggi;
+  }
+}
+
+class Balok extends BangunRuang{
+  Balok(int p, int l, int t)
+  {
+    panjang = p;
+    lebar = l;
+    tinggi = t;
+  }
+  @override
+  void hitungVolume() {
+    volume = panjang * lebar * tinggi;
+  }
+}
+
+class Kubus extends BangunRuang{
+  int sisi = 0;
+  Kubus(int s)
+  {
+    sisi = s;
+  }
+  @override
+  void hitungVolume() {
+    volume = sisi * sisi * sisi;
+  }
+}
+
+void main() {
+  Balok balok = Balok(25, 10, 5);
+  balok.hitungVolume();
+  print(balok.volume);
+
+  Kubus kubus = Kubus(5);
+  kubus.hitungVolume();
+  print(kubus.volume);
+}
